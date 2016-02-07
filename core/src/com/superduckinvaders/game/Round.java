2b8231cffd6d8e080388e098c7a9af0b577b53c2
bspammer <cc1311@york.ac.uk>
2016-02-07 23:30:42 +0000

    Some local testing changes that shouldn't have been pushed last commit

--- core/src/com/superduckinvaders/game/Round.java
+++ core/src/com/superduckinvaders/game/Round.java
@@ -138,13 +138,12 @@ public final class Round {
      * @param maxY the maximum y distance from the player to spawn the mobs
      */
     private void spawnRandomMobs(int amount, int minX, int minY, int maxX, int maxY) {
-    	createMob(getPlayer().getX() + 40, getPlayer().getY(), 200, Assets.badGuyNormal, 100);
-//        while(amount > 0) {
-//            int x = MathUtils.random(minX, maxX) * (MathUtils.randomBoolean() ? -1 : 1);
-//            int y = MathUtils.random(minY, maxY) * (MathUtils.randomBoolean() ? -1 : 1);
-//
-//            amount -= createMob(getPlayer().getX() + x, getPlayer().getY() + y, 100, Assets.badGuyNormal, 100) ? 1 : 0;
-//        }
+        while(amount > 0) {
+            int x = MathUtils.random(minX, maxX) * (MathUtils.randomBoolean() ? -1 : 1);
+            int y = MathUtils.random(minY, maxY) * (MathUtils.randomBoolean() ? -1 : 1);
+
+            amount -= createMob(getPlayer().getX() + x, getPlayer().getY() + y, 100, Assets.badGuyNormal, 100) ? 1 : 0;
+        }
     }
 
     /**
